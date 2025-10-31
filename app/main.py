eb75230e2870c475cc5cca887dfede17ac18178a
MantisEDR8 <javidelrio81@hotmail.com>
2025-10-31 14:05:22 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -396,5 +396,6 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app.services.main:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
 
